6142c15e4671815c1255fddad58b3861b9f8a346
Silvan <38138071+Sillyvan@users.noreply.github.com>
2025-02-26 18:18:34 +0100

    less html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,7 @@
 	import Mail from 'lucide-svelte/icons/mail';
 	import Phone from 'lucide-svelte/icons/phone';
 </script>
-			<div>
-				<h3 class="border-l border-amber-600 pl-6 font-semibold text-gray-900">Unser Standort</h3>
-				<address class="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
-					<p>4556 Brendan Ferry</p>
-					<p>Los Angeles, CA 90210</p>
-					<p class="mt-4 flex gap-1"><Phone /> (213) 555-0123</p>
-					<p class="mt-1 flex gap-1"><Mail /> info@yourbarber.com</p>
-				</address>
-			</div>
+<address class="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
+  <p class="mt-4 flex gap-1"><Phone /> (213) 555-0123</p>
+  <p class="mt-1 flex gap-1"><Mail /> info@yourbarber.com</p>
+</address>
